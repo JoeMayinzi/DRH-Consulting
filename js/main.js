3a6b7e78c672e5bd297d393890006875c55e8079
Joe Mayinzi <joel.mayinzi@groukam.com>
2022-06-08 12:35:35 +0200

    Codage du popup de securisation de la page admin

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 const createArticleLink = document.querySelector(".create-art-link");
+const popup = document.querySelector(".Modal");
+const form = document.querySelector("form");
+const passwordInput = document.querySelector("form input");
+const password = 123456789;
+
+
+popup.style.display = "none"
 
 const Modal = () => {
-    const div = document.createElement("div");
-    const msg = document.createElement("p")
-    msg.append(div)
+    popup.style.display = "block";
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault()
+        if (passwordInput.value == password) {
+            location.assign("http://127.0.0.1:5500/admin/admin.html");
+        } else {
+           location.assign("http://127.0.0.1:5500/index.html?") 
+        }
+        console.log(passwordInput.value)
 
-    div.classList.add("modal")
-    console.log(msg)
+    })
+
     
 }
 
